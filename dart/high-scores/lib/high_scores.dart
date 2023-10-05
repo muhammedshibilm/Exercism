4e7aa0d1a658fce99a6079ca5cd41d84f8166d2c
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2023-10-05 20:16:44 +0530

    The dart High-Score challenge is completed

--- dart/high-scores/lib/high_scores.dart
+++ dart/high-scores/lib/high_scores.dart
@@ -1,19 +1,39 @@
 class HighScores {
- fina List<int> scores;
+  final List<int> scores;
 
   HighScores(this.scores);
-
-  
 
   int latest() {
     return scores.last;
   }
 
   int personalBest() {
-    return scores.first;
+    int a;
+    for (int i = 0; i < scores.length; i++) {
+      for (var j = i + 1; j < scores.length; j++) {
+        if (scores[i] > scores[j]) {
+          a = scores[i];
+          scores[i] = scores[j];
+          scores[j] = a;
+        }
+      }
+    }
+
+    return scores.last;
   }
 
   List<int> personalTopThree() {
-    return sorted.take(3).toList();
+    int a;
+    for (int i = 0; i < scores.length; i++) {
+      for (var j = i + 1; j < scores.length; j++) {
+        if (scores[i] > scores[j]) {
+          a = scores[i];
+          scores[i] = scores[j];
+          scores[j] = a;
+        }
+      }
+    }
+
+    return scores.skip(scores.length - 3).toList();
   }
 }
